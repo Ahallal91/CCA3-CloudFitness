05cb7795880d0f40853b9d077c5556adc6e8de0c
Alicia Hallal <s3811836@student.rmit.edu.au>
2021-05-28 17:17:31 +1000

    added admin project

---
+++ admin-project/app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+def create_app():
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.secret_key = "some_key"
+
+    from .home import home
+    from .session import login
+    from .session import logout
+
+    app.register_blueprint(home.home_bp)
+    app.register_blueprint(login.login_bp)
+    app.register_blueprint(logout.logout_bp)
+    
+    return app
